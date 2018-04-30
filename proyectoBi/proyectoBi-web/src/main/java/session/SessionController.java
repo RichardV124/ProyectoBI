19bad114ed2b2e815064591508da02efbb1b6f96
Richard Vanegas <richardvanegas8@hotmail.com>
2018-04-30 03:41:54 -0500

    GGGGGGG

--- proyectoBi/proyectoBi-web/src/main/java/session/SessionController.java
+++ proyectoBi/proyectoBi-web/src/main/java/session/SessionController.java
@@ -13,37 +13,31 @@ import org.omnifaces.util.Messages;
 
 import beans.AccesoTipoUsuarioEJB;
 import beans.ConexionEJB;
-import beans.LoginEJB;
-import beans.TipoUsuarioEJB;
 import beans.UsuarioEJB;
 import beansSeguridad.SeguridadEJB;
 import entidades.Acceso;
-import entidades.Login;
 import entidades.Usuario;
-
 
 @Named("sessionController")
 @SessionScoped
 public class SessionController implements Serializable {
-	
+
 	@EJB
 	private SeguridadEJB seguridadEJB;
-	
+
 	@EJB
 	private UsuarioEJB usuarioEJB;
-	
+
 	@EJB
 	private AccesoTipoUsuarioEJB accesoTipoUsuarioEJB;
-	
-	
-	
+
 	@EJB
 	private ConexionEJB conexionEJB;
-	
+
 	private String username;
-	
+
 	private String password;
-	
+
 	/**
 	 * Es la base de datos que el sistema esta usando actualmente
 	 */
@@ -53,66 +47,78 @@ public class SessionController implements Serializable {
 	 * El usuario que ha iniciado sesion en la aplicacion
 	 */
 	private Usuario usuario;
-	
+
 	/**
 	 * Listado de accesos que tiene el usuario que inicio sesion
 	 */
 	private List<Acceso> accesos;
-	
+
 	/**
 	 * Iniciar sesion
 	 */
 	public String login() {
 		Usuario usu = usuarioEJB.buscarPorUsername(username, 2);
-		 
+
 		if (usu != null) {
-			
+
 			usuario = usu;
-			 
-			if(usu.getLogin().getPassword().equals(password)){
-				
-				if(usu.getTipoUsuario().getId()== 1){ // administrador
-					
+
+			if (usu.getLogin().getPassword().equals(password)) {
+
+				if (usu.getTipoUsuario().getId() == 1) { // administrador
+
 					Faces.setSessionAttribute("user", usuario);
 					System.out.println("Inicio sesion administrador");
-					
-					 // roles = rolEJB.ListaRolesPersona(persona.getIdentificacion());
-	                  accesos = accesoTipoUsuarioEJB.listarAccesosPorTipo(usu.getTipoUsuario(),bd);
 
-	                return "/paginas/seguro/administrador/inicioAdministrador.xhtml?faces-redirect=true";
-										
-				}else if(usu.getTipoUsuario().getId() == 2){ // cliente
+					accesos = accesoTipoUsuarioEJB.listarAccesosPorTipo(usu.getTipoUsuario(), bd);
+					bd = conexionEJB.buscar(1, bd).getId();
+					Faces.setSessionAttribute("bd", bd);
+
+					Messages.addFlashGlobalInfo("Ha iniciado sesion correctamente");
+
+					return "/paginas/seguro/administrador/inicioAdministrador.xhtml?faces-redirect=true";
+
+				} else if (usu.getTipoUsuario().getId() == 2) { // cliente
 					Faces.setSessionAttribute("user", usuario);
 					System.out.println("Inicio sesion cliente");
-					
-					// roles = rolEJB.ListaRolesPersona(persona.getIdentificacion());
-	                  accesos = accesoTipoUsuarioEJB.listarAccesosPorTipo(usu.getTipoUsuario(),bd);
-					
+
+					accesos = accesoTipoUsuarioEJB.listarAccesosPorTipo(usu.getTipoUsuario(), bd);
+
+					bd = conexionEJB.buscar(1, bd).getId();
+					Faces.setSessionAttribute("bd", bd);
+
+					Messages.addFlashGlobalInfo("Ha iniciado sesion correctamente");
+
 					return "/paginas/seguro/administrador/inicioCliente.xhtml?faces-redirect=true";
-					
-				}else if(usu.getTipoUsuario().getId() == 3){ // el otro
+
+				} else if (usu.getTipoUsuario().getId() == 3) { // el otro
 					Faces.setSessionAttribute("user", usuario);
 					System.out.println("Inicio sesion medico");
-					
-					// roles = rolEJB.ListaRolesPersona(persona.getIdentificacion());
-	                  accesos = accesoTipoUsuarioEJB.listarAccesosPorTipo(usu.getTipoUsuario(),bd);
-					
+
+					accesos = accesoTipoUsuarioEJB.listarAccesosPorTipo(usu.getTipoUsuario(), bd);
+
+					bd = conexionEJB.buscar(1, bd).getId();
+					Faces.setSessionAttribute("bd", bd);
+
+					Messages.addFlashGlobalInfo("Ha iniciado sesion correctamente");
+
 					return "/paginas/seguro/medico/citaMedico.xhtml?faces-redirect=true";
-					
-				}else{
+
+				} else {
 					Messages.addFlashGlobalError("Usuario o Password incorrectos");
 				}
-			}else{
+			} else {
 				Messages.addFlashGlobalError("Usuario o Password incorrectos");
 			}
-		}else{
+		} else {
 			Messages.addFlashGlobalError("Usuario o Password incorrectos");
 		}
 		return null;
 	}
-	
+
 	/**
 	 * Cierra la sesion en la aplicacion
+	 * 
 	 * @return la pagina de login
 	 */
 	public String cerrarSesion() {
@@ -122,12 +128,11 @@ public class SessionController implements Serializable {
 		sesion.invalidate();
 		return "/paginas/publico/login.xhtml?faces-redirect=true";
 	}
-	
+
 	public boolean isSesion() {
 		return usuario != null;
 	}
-	
-	
+
 	/**
 	 * @return the accesos
 	 */
@@ -136,7 +141,8 @@ public class SessionController implements Serializable {
 	}
 
 	/**
-	 * @param accesos the accesos to set
+	 * @param accesos
+	 *            the accesos to set
 	 */
 	public void setAccesos(List<Acceso> accesos) {
 		this.accesos = accesos;
@@ -150,7 +156,8 @@ public class SessionController implements Serializable {
 	}
 
 	/**
-	 * @param bd the bd to set
+	 * @param bd
+	 *            the bd to set
 	 */
 	public void setBd(int bd) {
 		this.bd = bd;
@@ -178,8 +185,6 @@ public class SessionController implements Serializable {
 
 	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
-	}	
-	
-	
-	
+	}
+
 }
